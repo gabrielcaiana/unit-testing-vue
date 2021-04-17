f70b03f93c00e81c7913842aeedb6eb87fdc72b4
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-04-17 15:53:47 -0300

    criando mais testes para o AppHeader

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,21 +1,27 @@
 import { mount } from '@vue/test-utils';
-import AppHeader from '@/components/AppHeader.vue'
+import AppHeader from '@/components/AppHeader.vue';
 
 describe('AppHeader', () => {
-  test('Verificando se existe um botão no header', () => {
-    const wrapper = mount(AppHeader)
-    expect(wrapper.contains('button')).toBe(true)
+  it('Verificando se existe um botão no header', () => {
+    const wrapper = mount(AppHeader);
+    expect(wrapper.contains('button')).toBe(true);
+  })
+
+  it('se o usuário não estiver logado, o botão deve estar invisivel', () => {
+    const wrapper = mount(AppHeader);
+    expect(wrapper.find('button').isVisible()).toBe(false);
   });
 
-  test('if user is not logged in, do not show logout button', () => {
-    const wrapper = mount(AppHeader)
-    expect(wrapper.find('button').isVisible()).toBe(false)
-  });
+  it('se o usuário estiver logado, o botão deve estar visivel', async () => {
+    const wrapper = mount(AppHeader);
+    wrapper.setData({ loggedIn: true });
+    await wrapper.vm.$nextTick;
+    expect(wrapper.find('button').isVisible()).toBe(true);
+  })
 
-    test('if user is logged in, show logout button', async () => {
-      const wrapper = mount(AppHeader)
-      wrapper.setData({ loggedIn: true})
-      await wrapper.vm.$nextTick
-      expect(wrapper.find('button').isVisible()).toBe(true)
-    })
+  it('deve conter o texto logout dentro do botão', () => {
+    const wrapper = mount(AppHeader)
+    const text = wrapper.find('button').element.textContent
+    expect(text).toContain('LoSagout')
+  })
 });
