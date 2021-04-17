d4362c457e46f085e01796a737a88a617ead923b
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-04-17 16:30:08 -0300

    criando teste recebendo props

--- tests/unit/RandomNumber.spec.js
+++ tests/unit/RandomNumber.spec.js
@@ -7,11 +7,29 @@ describe('RandomNumber', () => {
     expect(wrapper.html()).toContain('<span>0</span>')
   })
 
-  it('Se o botão for clicado, randomNumber deve estar entre 1 e 10', () => {
-    expect(true).toBe(true)
+  it('Se o botão for clicado, randomNumber deve estar entre 1 e 10', async () => {
+    const wrapper = mount(RandomNumber)
+    wrapper.find('button').trigger('click')
+
+    await wrapper.vm.$nextTick()
+
+    const randomNumber = parseInt(wrapper.find('span').element.textContent)
+    expect(randomNumber).toBeGreaterThanOrEqual(1)
+    expect(randomNumber).toBeLessThanOrEqual(10)
   })
 
-  it('Se o botão for clicado, randomNumber deve estar entre 200 e 300', () => {
-    expect(true).toBe(true)
+  it('Se o botão for clicado, randomNumber deve estar entre 200 e 300', async () => {
+    const wrapper = mount(RandomNumber, {
+        propsData: {
+            min: 200,
+            max: 300
+        }
+    })
+    wrapper.find('button').trigger('click')
+    await wrapper.vm.$nextTick()
+
+    const randomNumber = parseInt(wrapper.find('span').element.textContent)
+    expect(randomNumber).toBeGreaterThanOrEqual(200)
+    expect(randomNumber).toBeLessThanOrEqual(300)
   })
 })
