98d3a927f7590b6adedb5935cb543d195f6a5953
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-04-17 16:05:35 -0300

    criando estrutura de testes do componente RandomNumber

---
+++ tests/unit/RandomNumber.spec.js
@@ -0,0 +1,16 @@
+import { mount } from '@vue/test-utils';
+import RandomNumber from '@/components/RandomNumber';
+
+describe('RandomNumber', () => {
+  it('por padrão o valor deve ser 0', () => {
+    expect(true).toBe(true);
+  });
+
+  it('Se o botão for clicado, randomNumber deve estar entre 1 e 10', () => {
+    expect(true).toBe(true);
+  });
+
+  it('Se o botão for clicado, randomNumber deve estar entre 200 e 300', () => {
+    expect(true).toBe(true);
+  });
+});
